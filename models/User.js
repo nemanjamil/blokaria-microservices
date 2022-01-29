d10415d8de414b7ea60853e663cf217e0f2a1524
Nemanja Mili <nemanjamil@gmail.com>
2022-01-29 16:49:06 +0100

    Update Emails

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
 	userPassword: { type: String, required: true },
 	userVerified: { type: Number, required: true, default: 1 },
 	date: { type: Date, default: Date.now },
+	numberOfTransaction: { type: Number, required: true, default: parseInt(process.env.NUMBER_OF_TRANSACTIONS) },
 });
 
 module.exports = mongoose.model("User", userSchema);
