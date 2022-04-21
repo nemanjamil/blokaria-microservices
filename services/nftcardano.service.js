0850dbfbc2e566169f9b8c6ecbbc6ea81ec4b0b4
Nemanja Mili <nemanjamil@gmail.com>
2022-04-21 06:55:18 +0200

    Fix1

--- services/nftcardano.service.js
+++ services/nftcardano.service.js
@@ -2,12 +2,12 @@
 const DbService = require("moleculer-db");
 const dbConnection = require("../utils/dbConnection");
 const Nftcardano = require("../models/Nftcardano");
-const Cardano = require("../mixins/cardano.mixin");
+//const Cardano = require("../mixins/cardano.mixin");
 
 
 module.exports = {
     name: "nftcardano",
-    mixins: [DbService, Cardano],
+    mixins: [DbService], // Cardano
     adapter: dbConnection.getMongooseAdapter(),
     model: Nftcardano,
 
