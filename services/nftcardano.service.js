2b622e00665559eebb1ca6c733f0843ec1ba7934
Nemanja Mili <nemanjamil@gmail.com>
2022-04-22 09:58:19 +0200

    Test Initial NFT

--- services/nftcardano.service.js
+++ services/nftcardano.service.js
@@ -1,27 +1,29 @@
 "use strict";
 const DbService = require("moleculer-db");
 const dbConnection = require("../utils/dbConnection");
+const axiosMixin = require("../mixins/axios.mixin");
 const Nftcardano = require("../models/Nftcardano");
-//const Cardano = require("../mixins/cardano.mixin");
-
 
 module.exports = {
     name: "nftcardano",
-    mixins: [DbService], // Cardano
+    mixins: [DbService, axiosMixin], // Cardano
     adapter: dbConnection.getMongooseAdapter(),
     model: Nftcardano,
 
     actions: {
         createCardanoNft: {
-            async handler(ctx) {
+            async handler() {
+                let dataObject = {
+                    opala: "jedan"
+                };
                 try {
-                    return "aaa";
+                    let cardanoNftRequest = await this.axiosPost("172.20.0.1:3333", dataObject);
+                    return cardanoNftRequest;
                 } catch (error) {
                     return Promise.reject(error);
                 }
             },
         }
-
     },
 
     methods: {},
