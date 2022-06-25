4515f2c36c442b136800d98f0836e995d0c9a0cf
mihajlo1604 <35923303+mihajlo1604@users.noreply.github.com>
2022-06-25 18:17:17 +0200

    amountVal 2

--- services/wallets.service.js
+++ services/wallets.service.js
@@ -122,7 +122,7 @@ module.exports = {
 							assetId: qrCodeStatus[0]._nfts[0].assetId,
 							addressWallet: qrCodeStatus[0].nftsendaddress,
 							walletName: "NFT_TEST",
-							amountValue: 1.7,
+							amountValue: 2,
 						};
 
 						console.log("Wallet > WalletMinting NftParams", nftParams);
