28ef3b2cec540a5aabcda8a9fe44871ae9eeca16
Nemanja Mili <nemanjamil@gmail.com>
2022-08-27 19:45:20 +0200

    testnet-docker

--- moleculer.config.js
+++ moleculer.config.js
@@ -68,7 +68,7 @@ module.exports = {
 				ddSource: "moleculer-source-logger",
 
 				// Datadog env variable
-				env: "testNet-Docker",
+				env: "testnet-docker",
 				version: "1.0.0",
 				service: "moleculer-service-tracing",
 
@@ -268,7 +268,7 @@ module.exports = {
 					//tracer
 					agentUrl: "http://datadog-agent-on-docker:8126",
 					// Environment variable
-					env: "testNet-Docker",
+					env: "testnet-docker",
 					// Sampling priority. More info: https://docs.datadoghq.com/tracing/guide/trace_sampling_and_storage/?tab=java#sampling-rules
 					samplingPriority: "AUTO_KEEP",
 					// Default tags. They will be added into all span tags.
