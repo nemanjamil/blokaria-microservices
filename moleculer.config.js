c724ae1f8199c544f4c5ec04a1e7c624814d8466
Nemanja Mili <nemanjamil@gmail.com>
2022-08-27 17:39:55 +0200

    hostname

--- moleculer.config.js
+++ moleculer.config.js
@@ -276,7 +276,7 @@ module.exports = {
 					defaultTags: { "tracingTag1": "tag1", "tracingTag2": "tag2" },
 					// Custom Datadog Tracer options. More info: https://datadog.github.io/dd-trace-js/#tracer-settings
 					tracerOptions: {
-						//"hostname": "datadog-agent-on-docker",
+						"hostname": "datadog-agent-on-docker",
 						"service": "moleculer-service-tracing",
 						"logInjection": true,
 					},
