43061c080724262d847a0b66aa60a9a73f7f1f44
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-07-27 17:47:35 +0200

    Test Logs

--- services/nftcardano.service.js
+++ services/nftcardano.service.js
@@ -199,7 +199,7 @@ module.exports = {
 			async handler(ctx) {
 				try {
 					console.log("checkWallet ctx.params: call ", ctx.params);
-					console.log("checkWallet process.env.DOCKER_INTERNAL_URL ", process.env.DOCKER_INTERNAL_URL);
+					console.log("checkWallet process.env.DOCKER_INTERNAL_URL V2", process.env.DOCKER_INTERNAL_URL);
 
 					let checkWallet = await this.axiosPost(`${process.env.DOCKER_INTERNAL_URL}checkWallet`, ctx.params);
 
