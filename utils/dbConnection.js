458f2360eeddaa54699c6c6cd2e85435a1236a8a
LandaMm <qwer.009771@gmail.com>
2024-08-20 21:52:07 +0200

    feat: remove deprecated options mongo

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,10 +1,10 @@
 const MongooseAdapter = require("moleculer-db-adapter-mongoose");
 const dbConnection = {
 	getMongooseAdapter() {
+		console.log("mongoose adapter connection to:", process.env.MONGO_URL);
 		return new MongooseAdapter(`${process.env.MONGO_URL}/blokariawallet`, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
+			// useNewUrlParser: true,
+			// useUnifiedTopology: true,
 			user: process.env.MONGO_USERNAME,
 			pass: process.env.MONGO_PASSWORD,
 			keepAlive: true,
