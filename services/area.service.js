66be75018cbea098b4922c0fb5eaed72fe020805
egyptianego17 <abdulrahman.omar17h@gmail.com>
2024-09-22 22:16:30 +0300

    fix isPlanted

--- services/area.service.js
+++ services/area.service.js
@@ -339,7 +339,7 @@ const areaService = {
 							wallet.latitude = null;
 							wallet.longitude = null;
 							wallet.isPlanted = false;
-							if (wallet.geoLocation !== null || wallet.geoLocation.length != 0) {
+							if (wallet.geoLocation !== null && wallet.geoLocation !== "") {
 								wallet.isPlanted = true;
 								const [latitude, longitude] = wallet.geoLocation.split(",").map(coord => parseFloat(coord.trim()));
 								wallet.latitude = latitude;
