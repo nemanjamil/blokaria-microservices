7b2e7eb3a78e100375a40c384b3ac3b0f5b07531
Nemanja Milivojevic <nemanjamil@gmail.com>
2024-10-05 22:57:41 +0200

    Update Achievements

--- data/achievement.js
+++ data/achievement.js
@@ -3,14 +3,16 @@ module.exports = {
 		{
 			name: "Seed Planter",
 			description: "Achieve this level by planting your first tree.",
+			level: 1,
 			image: {
 				passive: "levels/lvl-1-b&w.png",
 				completed: "levels/lvl-1.png"
 			}
 		},
 		{
-			name: "Sapling Starter",
+			name: "Forest First",
 			description: "Reach this level by planting 5 trees.",
+			level: 2,
 			image: {
 				passive: "levels/lvl-2-b&w.png",
 				completed: "levels/lvl-2.png"
@@ -19,6 +21,7 @@ module.exports = {
 		{
 			name: "Growth Supporter",
 			description: "Unlock this level by planting 10 trees.",
+			level: 3,
 			image: {
 				passive: "levels/lvl-3-b&w.png",
 				completed: "levels/lvl-3.png"
@@ -27,30 +30,34 @@ module.exports = {
 		{
 			name: "Tree Guardian",
 			description: "Become a Tree Guardian by planting 15 trees.",
+			level: 4,
 			image: {
-				passive: "levels/LVL-4_gray_v2.png",
-				completed: "levels/LVL-4_colo_v2.png"
+				passive: "levels/LVL-4_gray_v3.png",
+				completed: "levels/LVL-4_colo_v3.png"
 			}
 		},
 		{
 			name: "Forest Protector",
 			description: "Reach this level by planting 20 trees.",
+			level: 5,
 			image: {
-				passive: "levels/LVL-5_gray_v2.png",
-				completed: "levels/LVL-5_colo_v2.png"
+				passive: "levels/LVL-5_gray_v3.png",
+				completed: "levels/LVL-5_colo_v3.png"
 			}
 		},
 		{
 			name: "Ecosystem Champion",
 			description: "Become an Ecosystem Champion by planting 30 trees.",
+			level: 6,
 			image: {
-				passive: "levels/LVL-6_gray_v2.png",
-				completed: "levels/LVL-6_colo_v2.png"
+				passive: "levels/LVL-6_gray_v3.png",
+				completed: "levels/LVL-6_colo_v3.png"
 			}
 		},
 		{
 			name: "Carbon Neutral - Climate Defender",
 			description: "Achieve Carbon Neutral status by planting 40 trees.",
+			level: 7,
 			image: {
 				passive: "levels/carbon-neutral_gray.png",
 				completed: "levels/carbon-neutral_color.jpg"
--- data/levels.js
+++ data/levels.js
@@ -3,36 +3,43 @@ module.exports = {
 		{
 			_id: "66e0318cfb6f7815a5cafe14",
 			name: "Seed Planter",
+			level: 1,
 			required_trees: 1
 		},
 		{
 			_id: "66e0223ef465873db5fa753e",
-			name: "Sapling Starter",
+			name: "Forest First",
+			level: 2,
 			required_trees: 5
 		},
 		{
 			_id: "66e02247f465873db5fa7541",
 			name: "Growth Supporter",
+			level: 3,
 			required_trees: 10
 		},
 		{
 			_id: "66e02251f465873db5fa7544",
 			name: "Tree Guardian",
+			level: 4,
 			required_trees: 15
 		},
 		{
 			_id: "66e02286f465873db5fa7547",
 			name: "Forest Protector",
+			level: 5,
 			required_trees: 20
 		},
 		{
 			_id: "66e02292f465873db5fa754a",
 			name: "Ecosystem Champion",
+			level: 6,
 			required_trees: 30
 		},
 		{
 			_id: "66e022a0f465873db5fa754d",
 			name: "Carbon Neutral - Climate Defender",
+			level: 7,
 			required_trees: 40
 		}
 	]
